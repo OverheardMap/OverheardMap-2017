7a52c4b408b01d1e025c77eed39e602adec4e4bf
Jerome Toole <jerometoole@gmail.com>
2017-11-06 14:53:08 +0000

    get drag scrolling working.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,64 +1,11 @@
-audiojs.events.ready(function() {
-
-var as = audiojs.createAll({
-	css: false,
-  createPlayer: {
-    markup: false,
-    playPauseClass: 'play-pause',
-    scrubberClass: 'scrubber',
-    progressClass: 'progress',
-    loaderClass: 'loaded',
-    timeClass: 'time',
-    durationClass: 'duration',
-    playedClass: 'played',
-    errorMessageClass: 'error-message',
-    playingClass: 'playing',
-    loadingClass: 'loading',
-    errorClass: 'error'
-	}
-  });
-
-});
-    
-
-$(document).ready(function () {
-  $('.accordion-tabs').each(function(index) {
-    $(this).children('li').children('a').next().hide();
-    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
-  });
-  $('.accordion-tabs').on('click', 'li > a.tab-link', function(event) {
-    if (!$(this).hasClass('is-active')) {
-      event.preventDefault();
-      var accordionTabs = $(this).closest('.accordion-tabs');
-      accordionTabs.find('.is-open').removeClass('is-open').hide();
-
-      $(this).next().toggleClass('is-open').fadeToggle('fast');
-      accordionTabs.find('.is-active').removeClass('is-active');
-      $(this).addClass('is-active');
-    } else {
-      event.preventDefault();
-    }
-  });
-});
-
 $(document).ready(function() {
-    $(window).scroll( function(){
-        $('.js-fademe').each( function(i){
-            
-            var object70height = $(this).outerHeight() - $(this).outerHeight() * 0.3;
-            var objectBottom = $(this).position().top + object70height;
-            var objectTop = $(this).position().top;
-            var windowBottom = $(window).scrollTop() + $(window).height();
-            var windowTop = $(window).scrollTop();
-            
-            if( windowBottom > objectBottom ){
-                $(this).addClass('js-fade-element-show');
-            } 
-            else if ( windowBottom < objectTop ) {
-                $(this).removeClass('js-fade-element-show');
-                $(this).addClass('js-fade-element-hide');
-            }
+  var width = $(window).width();
+  var height = $(window).height();
+  $('body').scrollTo('50%', 800);
 
-        }); 
-    });
+  $('.centerViewport').click(function(event) {
+    console.log('click');
+    $('body').scrollTo('50%', 800);
+  });
 });
+
--- gulpfile.js
+++ gulpfile.js
@@ -95,9 +95,7 @@ var cssTasks = function(filename) {
     .pipe(concat, filename)
     .pipe(autoprefixer, {
       browsers: [
-        'last 2 versions',
-        'android 4',
-        'opera 12'
+        'last 5 versions'
       ]
     })
     .pipe(minifyCss, {
