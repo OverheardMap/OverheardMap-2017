626bbeb66e79ae932df5551bcd3b0e183f536358
Jerome Toole <jerometoole@gmail.com>
2017-11-06 18:46:48 +0000

    add coffee date and like interaction

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
   var width = $(window).width();
   var height = $(window).height();
-  $('body').scrollTo('50%', 800);
+  $('body').scrollTo('50%');
 
   $('.centerViewport').click(function(event) {
     console.log('click');
     $('body').scrollTo('50%', 800);
+  });
+
+  $('span.like').click(function(event) {
+    $(this).toggleClass('active');
   });
 });
 
