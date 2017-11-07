dfa5b62eeea9668651fb76deceb8fecdda767623
Jerome Toole <jerometoole@gmail.com>
2017-11-07 16:19:00 +0000

    add profiles

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,5 +14,57 @@ $(document).ready(function() {
     console.log('this');
     $('.friendly .card').toggleClass('flip');
   });
+
+
+
+
+});
+
+$(function () {
+  var adj = [
+  'Fun-Seeking',
+  'Family Stage',
+  'Trendy',
+  'Status Seeking',
+  'Conservative',
+  'Socially Responsible',
+  'Environmentally Conscious',
+  'Family Oriented',
+  'Technical'
+  ];
+
+  var noun = [
+  'Sports Enthusiast',
+  'Influencer',
+  'Homeowner',
+  'self actualizer',
+  'affluent achiever',
+  'thriving grey',
+  'settled suburban',
+  'nest builder',
+  'senior citizen',
+  'have-not',
+  'contented conformer',
+  'traditionalist'
+  ]
+
+
+  $('#theButton').click(function() {
+  });
+
+  var getAdjective = function() {
+    return adj[Math.floor(Math.random() * adj.length)];
+  };
+  var getNoun = function() {
+    return noun[Math.floor(Math.random() * noun.length)];
+  };
+
+  setInterval(function () {
+    $(".profiles__word").fadeOut(400, function () {
+      $('.adj').html(getAdjective());
+      $('.noun').html(getNoun());
+      $(this).fadeIn(400);
+    });
+  }, 3000);
 });
 
